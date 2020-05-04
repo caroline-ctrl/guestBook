66e25fff238eb6aa0768d79c91a0d461b426e6ce
caroline-ctrl <duon.caroline@gmail.com>
2020-05-04 14:55:21 +0200

    mise en place d'une exception

--- blog/index.php
+++ blog/index.php
@@ -1,19 +1,27 @@
 <?php
 $pdo = new PDO('sqlite:../data.db');
-$query = $pdo->query('SELECT * FROM posts');
-if ($query ===  false){
-    var_dump($pdo->errorInfo());
-    die('Erreur SQL');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$error = null;
+try {
+    $query = $pdo->query('SELECT * FROM posts');
+    $posts = $query->fetchAll(PDO::FETCH_OBJ);
+} catch (PDOException $e) {
+    $error = $e->getMessage();
 }
-$posts = $query->fetchAll(PDO::FETCH_OBJ);
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'header.php';
 ?>
 
-<ul>
-    <?php foreach($posts as $post): ?>
-        <li><?= $post->name ?></li>
-    <?php endforeach ?>
-</ul>
+<?php if ($error) : ?>
+    <div class="alert alert-danger">
+        <?= $error ?>
+    </div>
+<?php else : ?>
+    <ul>
+        <?php foreach ($posts as $post) : ?>
+            <li><?= $post->name ?></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
 
 
 <?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'footer.php'; ?>
