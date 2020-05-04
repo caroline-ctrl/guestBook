57b7a2d850d3c2aa8cc66ae2ebb4fc163fbda85d
caroline-ctrl <duon.caroline@gmail.com>
2020-05-04 14:48:17 +0200

    utilisation de FETCH_OBJ

--- blog/index.php
+++ blog/index.php
@@ -1,8 +1,19 @@
 <?php
+$pdo = new PDO('sqlite:../data.db');
+$query = $pdo->query('SELECT * FROM posts');
+if ($query ===  false){
+    var_dump($pdo->errorInfo());
+    die('Erreur SQL');
+}
+$posts = $query->fetchAll(PDO::FETCH_OBJ);
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'header.php';
 ?>
 
-
+<ul>
+    <?php foreach($posts as $post): ?>
+        <li><?= $post->name ?></li>
+    <?php endforeach ?>
+</ul>
 
 
 <?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'footer.php'; ?>
